3a03765efeda1785e89b30e3b9e8ecba7f151c76
Marios Isaakidis <prometheas@autistici.org>
2020-04-26 19:40:04 -0400

    Serve HTTP on biton app with express and pug

--- bin/biton.js
+++ bin/biton.js
@@ -1,15 +1,38 @@
 #!/usr/bin/env node
 
 const bitonClient = require('../')
+const express = require('express')
+const pug = require('pug')
+
+const PORT = process.env.PORT || 5000
+const HOST = '0.0.0.0'
 
 console.log('biton webtorrent-hybrid client')
 
 let client = new bitonClient()
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send(pug.render('p Hello world!'))
+})
+
+app.listen(PORT, () => {
+    console.log('HTTP server running at ' + HOST + ':' + PORT)
+})
+
+
+/*
+* Graceful shutdown. Close active connections. Delete logs and uncompleted chunks
+* */
 
 function exitHandler(options, exitCode) {
     console.log('\nDestroying biton wires..')
-    if (!client.destroyed)
-      client.destroy()
+    if (app.listening) {
+        app.close()
+    }
+    if (!client.destroyed) {
+        client.destroy()
+    }
     process.exit()
 }
 
